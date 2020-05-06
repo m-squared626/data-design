94a5d3b1568a098aed9e5e5c26fd4e78e50b19fb
Mauricio Madrid <maumadri@yahoo.com>
2020-05-06 05:47:06 -0600

    sql table

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -5,13 +5,13 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE article (
     articleId binary(16)not null,
-    articleAuthor binary(32) not null,
-    articleCategory binary(32) not null,
-    articleDate datetime(16) not null,
+    articleAuthor varchar(32) not null,
+    articleCategory varchar(32) not null,
+    articleDate datetime(3) not null,
     articleHash binary(32) not null,
-    articleProvider binary(32) not null,
-    articleSubject binary(32) not null,
-    articleTitle binary(32) not null,
+    articleProvider varchar(32) not null,
+    articleSubject varchar(32) not null,
+    articleTitle varchar(64) not null,
     primary key (articleId)
 );
 
@@ -19,23 +19,22 @@ CREATE TABLE user(
     userId binary(32) not null,
     userEmail binary(32)not null,
     userHash binary(32)not null,
-    userPhoneNum binary(32)not null
-    primary key (userId)
-    );
+    userPhoneNum numeric(16)not null
+);
 
 CREATE TABLE category(
     categoryId binary(16) not null,
     categoryHash binary(32) not null,
-    categoryProvider binary(32) not null,
-    categorySubject binary(32) not null,
-    categoryTitle binary(32) not null,
+    categoryProvider varchar(32) not null,
+    categorySubject varchar(32) not null,
+    categoryTitle varchar(32) not null,
     primary key(categoryId)
 );
 
 CREATE TABLE articleCategory(
     articleCategoryId binary(16) not null,
-    articleCategoryProvider binary(32) not null,
-    articleSubject binary(32) not null,
+    articleCategoryProvider varchar(32) not null,
+    articleSubject varchar(32) not null,
     primary key(articleCategoryId)
 );
 
