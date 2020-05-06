9ce507c2f4cfac2714907423c16227ca88751b12
Mauricio Madrid <maumadri@yahoo.com>
2020-05-06 05:24:36 -0600

    sql table

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,33 +1,41 @@
-DROP TABLE IF EXISTS articleCategory
-DROP TABLE IF EXISTS article
-DROP TABLE IF EXISTS category
-DROP TABLE IF EXISTS user
+DROP TABLE IF EXISTS articleCategory;
+DROP TABLE IF EXISTS article;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS user;
 
 CREATE TABLE article (
-    articleId(16)not null,
-    articleAuthor(32) not null,
-    articleCategory(32) not null,
-    articleDate
-    articleHash(32) not null,
-    articleProvider(32) not null,
-    articleSubject(32) not null,
-    articleTitle(32) not null,
+    articleId binary(16)not null,
+    articleAuthor binary(32) not null,
+    articleCategory binary(32) not null,
+    articleDate datetime(16) not null,
+    articleHash binary(32) not null,
+    articleProvider binary(32) not null,
+    articleSubject binary(32) not null,
+    articleTitle binary(32) not null,
     primary key (articleId)
-)
+);
+
+CREATE TABLE user(
+    userId binary(32) not null,
+    userEmail binary(32)not null,
+    userHash binary(32)not null,
+    userPhoneNum binary(32)not null
+    primary key (userId)
+    );
 
 CREATE TABLE category(
-    categoryId(16) not null,
-    categoryHash(32) not null,
-    categoryProvider(32) not null,
-    categorySubject(32) not null,
-    categoryTitle(32) not null,
+    categoryId binary(16) not null,
+    categoryHash binary(32) not null,
+    categoryProvider binary(32) not null,
+    categorySubject binary(32) not null,
+    categoryTitle binary(32) not null,
     primary key(categoryId)
-)
+);
 
 CREATE TABLE articleCategory(
-    articleCategoryId(16) not null,
-    articleCategoryProvider(32) not null,
-    articleSubject(32) not null,
+    articleCategoryId binary(16) not null,
+    articleCategoryProvider binary(32) not null,
+    articleSubject binary(32) not null,
     primary key(articleCategoryId)
-)
+);
 
